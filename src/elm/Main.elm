8758120198f547b77845c05e90daf7a57b26975c
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2023-01-11 13:13:09 -0500

    attempt to find out why breakpoints mixin doesn't work

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, href, id, target)
+import Html.Attributes exposing (alt, attribute, class, height, href, id, media, src, target, width)
 import Html.Events exposing (onClick, onSubmit)
 
 
@@ -57,7 +57,23 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div [ id "root" ]
-        [ footer [ class "attribution" ]
+        [ header [ class "header" ]
+            [ h1 [ class "sr-only" ] [ text "Interactive Card Details Form" ]
+            ]
+        , div [ class "wrapper" ]
+            -- card as in credit card, not a card component, thus no base card class
+            [ div [ class "card-image" ]
+                [ picture []
+                    [ source [ media "(min-width: 48em)", attribute "srcset" "./images/bg-main-desktop.png", width 483, height 900 ] []
+                    , source [ media "(min-width: 23.4375em)", attribute "srcset" "./images/bg-main-mobile.png", width 375, height 240 ] []
+                    , img [ src "./images/bg-main-mobile.png", alt "" ] []
+                    ]
+                ]
+            , div [ class "card-form" ]
+                [ img [ src "./images/bg-card-front.png" ] []
+                ]
+            ]
+        , footer [ class "attribution" ]
             [ p []
                 [ text "Challenge by "
                 , a [ href "https://www.frontendmentor.io?ref=challenge", target "_blank" ] [ text "Frontend Mentor" ]
@@ -66,6 +82,15 @@ view model =
                 ]
             ]
         ]
+
+
+
+-- picture element doesn't exist as of yet so this is a replacement
+
+
+picture : List (Html.Attribute msg) -> List (Html msg) -> Html msg
+picture =
+    Html.node "picture"
 
 
 
